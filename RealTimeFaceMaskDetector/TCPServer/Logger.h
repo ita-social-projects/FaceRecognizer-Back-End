e89412b6dfff5b88196b1735fd91e98a31b281d9
Andrew Polyanskiy <71780594+ppolyanskiyy@users.noreply.github.com>
2020-12-07 15:40:04 +0200

    Update Logger.h

--- RealTimeFaceMaskDetector/TCPServer/Logger.h
+++ RealTimeFaceMaskDetector/TCPServer/Logger.h
@@ -4,7 +4,7 @@
 INITIALIZE_EASYLOGGINGPP
 
 	const std::filesystem::path		LOG_FOLDER = R"(C:\ProgramData\RTFMD_Logs\)";
-	const std::string				CONFIGURATION_PATH = R"(Configuration.conf)";
+	const std::string				CONFIGURATION_PATH = R"(..\LoggerConfiguration.conf)";
 	const std::string				LOG_NAME = static_cast<std::string>(__DATE__) + "_log";
 	const std::string				LOG_EXTENSION = ".log";
 	const std::string				LOGGER_ID = "RTFMD";
