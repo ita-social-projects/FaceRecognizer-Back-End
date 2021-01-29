21676efe550f27219bd96fed2ca2240e5c204b4a
MarkiianYatsiv <61264740+marko-yaciv@users.noreply.github.com>
2021-01-29 12:49:21 +0200

    Ppolyanskiyy to dev (#4)
    * Full project added
    
    * Update Configuration.conf
    
    * Update Logger.h
    
    * Delete Configuration.conf
    
    * Update Logger.h
    
    * Update Configuration.conf
    
    * Add files via upload
    
    * Update Configuration.conf
    
    * Delete Configuration.conf
    
    Co-authored-by: Andrew Polyanskiy <ppolyanskiyy@gmail.com>
    Co-authored-by: Andrew Polyanskiy <71780594+ppolyanskiyy@users.noreply.github.com>

--- RealTimeFaceMaskDetector/EncryptDecryptECBMode/ICrypt.h
+++ RealTimeFaceMaskDetector/EncryptDecryptECBMode/ICrypt.h
@@ -4,6 +4,8 @@
 // that uses this DLL. This way any other project whose source files include this file see
 // ENCRYPTDECRYPTECBMODE_API functions as being imported from a DLL, whereas this DLL sees symbols
 // defined with this macro as being exported.
+#pragma once
+
 #ifdef ENCRYPTDECRYPTECBMODE_EXPORTS
 #define ENCRYPTDECRYPTECBMODE_API __declspec(dllexport)
 #else
