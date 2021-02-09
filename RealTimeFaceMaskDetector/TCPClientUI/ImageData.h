ed7c298011f6b4741bfe2934b3bf1e172f04c18a
Vladyslav Bielan <37587646+nullStandard@users.noreply.github.com>
2021-02-09 14:10:13 +0200

    ImageData class was added (#15)

---
+++ RealTimeFaceMaskDetector/TCPClientUI/ImageData.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+
+#include <vector>
+#include <iostream>
+#include <conio.h>	
+#include <thread>
+#include <algorithm>
+//using faceInfo = std::vector<std::pair<cv::Rect, bool>>;
+
+#define faceInfo std::vector<std::pair<cv::Rect, bool>>
+
+class ImageData
+{
+	cv::Mat m_image;
+	faceInfo m_faces;
+	std::mutex m_mtx;
+
+public:
+	void SetData(cv::Mat& image, faceInfo& faces);
+
+	void GetData(cv::Mat& image, faceInfo& faces);
+};
+
