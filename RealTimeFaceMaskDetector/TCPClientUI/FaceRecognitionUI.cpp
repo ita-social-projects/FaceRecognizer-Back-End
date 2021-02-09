8a25753d2abf3e35c1575264b44a48e317d29f0d
Vladyslav Bielan <vladyslav.bielan@gmail.com>
2021-02-09 22:23:29 +0200

    added multithreading, starting client

--- RealTimeFaceMaskDetector/TCPClientUI/FaceRecognitionUI.cpp
+++ RealTimeFaceMaskDetector/TCPClientUI/FaceRecognitionUI.cpp
@@ -1,6 +1,7 @@
+#pragma once
 #include "FaceRecognitionUI.h"
-
 #include <QDebug>
+#include <thread>
 
 FaceRecognitionUI::FaceRecognitionUI(QWidget* parent)
     : QWidget(parent)
@@ -12,53 +13,66 @@ FaceRecognitionUI::FaceRecognitionUI(QWidget* parent)
 void FaceRecognitionUI::onExitButtonClicked()
 {
     m_exit_button_clicked = true;
+    run_analizer = false;
     close();
     qDebug() << "QQQQ!!!";
 };
 
-void FaceRecognitionUI::UpdateWindow(TCPClient &client)
+void FaceRecognitionUI::updateWindow(TCPClient& client)
 {
-    
+
+    std::thread thrd(&FaceRecognitionUI::recognize, this, 0);
+
     while (!m_exit_button_clicked)
     {
-        m_mask_recognizer->RunAnalysis();
-
-        cv::VideoCapture m_camera;
+        /*cv::VideoCapture m_camera;
         if (!m_camera.open(0))
         {
             throw std::runtime_error("can't load camera");
-        }
+        }*/
         cv::Mat image;
-        m_camera >> image;
+        faceInfo faces;
+        //m_camera >> image;
 
-        QImage frame = Mat2QImage(image);
+        img_data.GetData(image, faces);
+
+        if (image.empty())
+        {
+            continue;
+        }
+
+        QImage frame = mat2QImage(image);
         QPixmap map = QPixmap::fromImage(frame.scaled(640, 480, Qt::KeepAspectRatio, Qt::FastTransformation));
         ui.frame->setPixmap(map);
         ui.frame->show();
         cv::waitKey(30);
-        std::vector<char> buffer;
+
+        /*std::vector<char> buffer;
         cv::imwrite("image_face.png", image);
 
         std::ifstream image_face_send;
         client.ConvertImageToBinary(image_face_send, buffer);
         client.SendBinaryMessage(buffer);
 
-        Sleep(2000);
+        sendImage();*/
+
+        //Sleep(2000);
+
         qDebug() << "QQQQ\n   ";
     }
 }
 
-void FaceRecognitionUI::Recognize(int camera_id)
+void FaceRecognitionUI::recognize(int camera_id)
 {
     FaceRecognizer recognizer(img_data, camera_id);
 
-    while (true)
+    while (run_analizer)
     {
-
+        recognizer.runAnalysis();
     }
 }
 
-QImage FaceRecognitionUI::Mat2QImage(cv::Mat const& src)
+QImage FaceRecognitionUI::mat2QImage(cv::Mat const& src)
 {
     cv::Mat temp; // make the same cv::Mat
     cvtColor(src, temp, cv::COLOR_BGR2RGB); // cvtColor Makes a copt, that what i need
@@ -67,9 +81,11 @@ QImage FaceRecognitionUI::Mat2QImage(cv::Mat const& src)
     // of QImage::QImage ( const uchar * data, int width, int height, Format format )
     return dest;
 }
-std::unique_ptr<FaceRecognizer> m_mask_recognizer = std::make_unique<FaceRecognizer>();
 
-
+void FaceRecognitionUI::sendImage()
+{
+    
+}
 
 
 FaceRecognitionUI::~FaceRecognitionUI()
--- RealTimeFaceMaskDetector/TCPClientUI/FaceRecognitionUI.h
+++ RealTimeFaceMaskDetector/TCPClientUI/FaceRecognitionUI.h
@@ -9,25 +9,25 @@ class FaceRecognitionUI : public QWidget
 {
 	Q_OBJECT
 
-public:
-	FaceRecognitionUI(QWidget *parent = Q_NULLPTR);
-	~FaceRecognitionUI();
-
-	bool m_exit_button_clicked = false;
-	void UpdateWindow(TCPClient &client);
-	QImage Mat2QImage(cv::Mat const& src);
-
-	void Recognize(int camera_id=0);
-
 public slots:
 	void onExitButtonClicked();
 
 public:
+	FaceRecognitionUI(QWidget *parent = Q_NULLPTR);
+
 	Ui::FaceRecognitionUI ui;
+
+	QImage mat2QImage(cv::Mat const& src);
+
+	void updateWindow(TCPClient& client);
+	void recognize(int camera_id);
+	void sendImage(); // not finished
+
+	~FaceRecognitionUI();
 
 private:
 	ImageData img_data;
+
+	bool m_exit_button_clicked = false;
 	bool run_analizer = true;
 };
-
-extern std::unique_ptr<FaceRecognizer> m_mask_recognizer;
