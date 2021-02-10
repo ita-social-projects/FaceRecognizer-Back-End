e6dfee8bc297e9e1e08e4e3320a68d3c8d3c4ffe
Vladyslav Bielan <37587646+nullStandard@users.noreply.github.com>
2021-02-10 10:16:31 +0200

    Fixing bug, which didn`t allow us start client for the first time. Now we can (#23)
    * ImageData class was added
    
    * added multithreading, starting client
    
    * "Fixing bug, which didn`t allow us start client for the first time"

--- RealTimeFaceMaskDetector/TCPClientUI/FaceRecognitionUI.cpp
+++ RealTimeFaceMaskDetector/TCPClientUI/FaceRecognitionUI.cpp
@@ -32,8 +32,8 @@ void FaceRecognitionUI::updateWindow(TCPClient& client)
         }*/
         cv::Mat image;
         faceInfo faces;
-        //m_camera >> image;
-
+        
+        //������� ����, ��� �������� � FaceRecognizer
         img_data.GetData(image, faces);
 
         if (image.empty())
@@ -64,11 +64,13 @@ void FaceRecognitionUI::updateWindow(TCPClient& client)
 
 void FaceRecognitionUI::recognize(int camera_id)
 {
-    FaceRecognizer recognizer(img_data, camera_id);
+    FaceRecognizer recognizer(camera_id);
 
     while (run_analizer)
     {
-        recognizer.runAnalysis();
+        // ���� ���������� FaceRecognitionUI::ImageData � FaceRecognizer, 
+        // ��� ��� ������ ����� ����, ���������� ���� ������
+        recognizer.runAnalysis(img_data);
     }
 }
 
