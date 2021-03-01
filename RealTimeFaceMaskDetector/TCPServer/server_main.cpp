aeb531a717b87346b253ba9d575d30050ac9c062
marko-yatsiv <2002mar4ik2002@gmail.com>
2021-03-01 17:42:01 +0200

    removed logs in sockets serverdestructor. Updated .gitignore to avoid config.xml

--- RealTimeFaceMaskDetector/TCPServer/server_main.cpp
+++ RealTimeFaceMaskDetector/TCPServer/server_main.cpp
@@ -13,8 +13,6 @@ void CreateServiceTableEntry(ServiceStarter& service, SERVICE_TABLE_ENTRY(*entry
 
 int wmain(unsigned argc,  wchar_t* argv[])
 {
-	LOG_MSG << "wmain: start";
-	
 	bool result = ChooseServerBootOptionAndStart(argc, argv);
 
 	LOG_MSG << "wmain: finish";
