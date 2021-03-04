4f76550355ff43b43de5e9a47b66ce6238195af7
Vladyslav Bielan <37587646+nullStandard@users.noreply.github.com>
2021-03-04 19:06:30 +0200

    Inc framerate (#59)
    * refactored faceRecognition to increase framerate
    
    * Adding a few comments

--- RealTimeFaceMaskDetector/TCPClientUI/FaceRecognitionUI.h
+++ RealTimeFaceMaskDetector/TCPClientUI/FaceRecognitionUI.h
@@ -1,9 +1,19 @@
 #pragma once
+#include <chrono>
+#include <QtCore/QDebug>
+#include <QtGui/QPainter>
+#include <thread>
+#include <utility>
+#include <future>
 
 #include "ui_FaceRecognitionUI.h"
 #include "FaceRecognizer.h"
 #include "TCPClient.h"
-#include "ImageData.h"
+#include "TimeCounting.h"
+
+#define IDCAM 0
+#define TIME_PERIOD 5
+
 
 class FaceRecognitionUI : public QWidget
 {
@@ -17,15 +27,17 @@ public:
 
 	Ui::FaceRecognitionUI ui;
 
-	QImage mat2QImage(cv::Mat const& src);
-
+	QImage mat2QImage();
+	void displayFrame();
 	void updateWindow(TCPClient& client);
-
-	void sendImage(TCPClient& client, cv::Mat img);
-
+	void sendImage(TCPClient& client, cv::Mat face_img);
+	bool is_ready(std::future<faceInfo> const& f);
+	void SetPanelText();
 	~FaceRecognitionUI();
 
 private:
+	cv::Mat m_image;
+	bool m_async_is_permitted = true;
+	bool m_is_all_in_mask = true;
 	bool m_exit_button_clicked = false;
-	bool run_analizer = true;
 };
